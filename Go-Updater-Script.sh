c76f273fcf54b803516d462cd7ec5865a7bb82ac
byte-sec <130414280+byte-sec@users.noreply.github.com>
2023-04-11 05:30:05 +0500

    Update Go-Updater-Script.sh

--- Go-Updater-Script.sh
+++ Go-Updater-Script.sh
@@ -9,14 +9,12 @@ wget https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
 tar -xzf go1.20.3.linux-amd64.tar.gz
 sudo cp -r go /usr/local
 
-# Update the PATH environment variable to include the new Go installation
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 source ~/.bashrc
 
 # Set the GOROOT and GOPATH environment variables
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go/bin      #default
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
 source ~/.bashrc
 
 # Verify that Go is installed and working correctly
